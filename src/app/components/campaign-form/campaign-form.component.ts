3e0086636899539e9b99646b9652c7484188b8a4
Abdessamad Bounasseh <Abdessamad.Bounasseh@capgemini.com>
2024-12-03 00:02:09 +0100

    handle update campaign

--- src/app/components/campaign-form/campaign-form.component.ts
+++ src/app/components/campaign-form/campaign-form.component.ts
@@ -1,16 +1,14 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {CampaignRequest} from "../../models/CampaignRequest";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {CampaignService} from "../../services/campaign.service";
 import {CampaignResponse} from "../../models/CampaignResponse";
-import {log} from "ng-zorro-antd/core/logger";
-import {Campaign} from "../../models/Campaign";
 import {AsyncPipe, NgClass} from "@angular/common";
 import {NzFormModule} from "ng-zorro-antd/form";
 import {NzInputDirective} from "ng-zorro-antd/input";
-import {ActivatedRoute, ROUTES} from "@angular/router";
-import {Observable} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
 import {HttpResponsePayload} from "../../payloads/HttpResponsePayload";
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzMessageService} from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-campaign-form',
@@ -20,7 +18,8 @@ import {HttpResponsePayload} from "../../payloads/HttpResponsePayload";
     NzFormModule,
     NgClass,
     NzInputDirective,
-    AsyncPipe
+    AsyncPipe,
+    NzButtonModule
   ],
   templateUrl: './campaign-form.component.html',
   styleUrl: './campaign-form.component.css'
@@ -29,10 +28,13 @@ export class CampaignFormComponent implements OnInit {
   campaignFormGroup!: FormGroup;
   isAddForm: boolean = true;
   campaignName!: string;
+  loadingMessageId!: string;
 
   campaignService = inject(CampaignService);
   formBuilder = inject(FormBuilder);
   route = inject(ActivatedRoute);
+  router = inject(Router);
+  messageService = inject(NzMessageService);
 
   ngOnInit() {
     this.campaignName = this.route.snapshot.params['name'];
@@ -57,17 +59,38 @@ export class CampaignFormComponent implements OnInit {
   }
 
   submit() {
+    this.createBasicMessage();
     if (this.isAddForm) {
       this.campaignService.createNewCampaign(this.campaignFormGroup.value).subscribe({
-        next: (res: HttpResponsePayload) => console.log(res.message),
-        error: err => console.log(err)
+        next: () => this.messageService.success('Campaign created successfully !'),
+        error: () => {
+          this.messageService.error('Failed to create campaign !');
+          this.messageService.remove(this.loadingMessageId);
+        },
+        complete: () => {
+          this.messageService.remove(this.loadingMessageId);
+          this.router.navigate(['campaigns']);
+        }
       });
     } else {
       this.campaignService.updateCampaign(this.campaignName, this.campaignFormGroup.value).subscribe({
-        next: () => console.log('Campaign updated successfully !'),
-        error: (err: HttpResponsePayload) => console.log(err.message)
+        next: () => this.messageService.success('Campaign updated successfully !'),
+        error: () => {
+          this.messageService.error('Failed to update campaign !');
+          this.messageService.remove(this.loadingMessageId);
+        },
+        complete: () => {
+          this.messageService.remove(this.loadingMessageId);
+          this.router.navigate(['campaigns']);
+        }
       });
     }
+  }
 
+  createBasicMessage(): void {
+    this.loadingMessageId = this.messageService.loading(
+      this.isAddForm ? 'Creation in progress...' : 'Update in progress...',
+      {nzDuration: 0}
+    ).messageId;
   }
 }
